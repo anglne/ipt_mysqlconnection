47d6a9eb1dd5a958cff2f0d75c5d30cee41f86c1
Angeline Delos Reyes <125114433+anglne@users.noreply.github.com>
2023-12-08 20:39:14 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using adr.myclass;
+
+
+namespace adr
+{
+    public partial class Form1 : Form
+    {
+        connection_class con = new connection_class();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.connectdb.Open();
+                label1.Text = "Connected.";
+                label1.ForeColor = Color.Green;
+                con.connectdb.Close();
+            }
+            catch (Exception)
+            {
+                label1.Text = "Failed to Connect Database.";
+            }
+        }
+    }
+}
---
+++ conncection_class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Cms;
+
+namespace adr.myclass
+{
+    internal class connection_class
+    {
+        public MySqlConnection connectdb;
+
+        public connection_class()
+        {
+
+            string host = "localhost";
+            string database = "new_schema";
+            string username = "root";
+            string password = "";
+            string port = "3306";
+
+            string connection_string = "datasource =" + host + ";database=" + database + "; port=" + port + "; username=" + username + ";" + "password=" + password + "; SslMode =none; ";
+
+            connectdb = new MySqlConnection(connection_string);
+
+        }
+
+
+    }
+}
